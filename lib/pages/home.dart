126c58d434970e23188d6e72d046c4cebab5c530
Qonus <94901082+Qonus@users.noreply.github.com>
2025-02-05 20:32:34 +0500

    [Update] delete resource: from image uri prefix

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return Container(
+            child: MarkdownBody(
+              selectable: true,
+              data: "widget.page.markdown",
+              imageBuilder: (uri, title, alt) {
+                final assetPath = uri.toString();
+                return Center(
+                  child: Image.asset(
+                    assetPath,
+                    fit: BoxFit.contain,
+                  ),
+                );
+              },
+              styleSheet: MarkdownStyleSheet(
+                h1: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+                h2: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+                h3: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+                p: TextStyle(
+                  fontSize: 17,
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+              ),
+            ),
+          );
   }
 }
